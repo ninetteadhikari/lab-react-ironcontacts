4f6ca778a1c8c7298f4f334bee21fe6446f03ad8
Ninette <ninette.adhikari@gmail.com>
2019-09-25 14:42:51 +0200

    with bonus

--- starter-code/src/Contact.js
+++ starter-code/src/Contact.js
@@ -1,35 +1,21 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
-  state = {
-    contact: this.props.contact
-  };
+  
 
-  deleteState = e => {
-    console.log(this.props.contact.indexOf(e))
 
-    let deleteIndex = this.props.contact.indexOf(e)
-    let newArr=[...this.props.contact]
-      if (deleteIndex !== -1) {
-        newArr.splice(deleteIndex, 1);
-        console.log(newArr)
-        this.setState({
-          contact: newArr
-        });
-  }
-  }
 
   render() {
     const contact = this.props.contact.map(data => {
       return (
         <tr key={data.id}>
           <td>
-            <img src={data.pictureUrl} height="200px" alt="" />
+            <img src={data.pictureUrl} height="100px" alt={data.name} />
           </td>
           <td>{data.name}</td>
-          <td>{data.popularity}</td>
+          <td>{data.popularity.toFixed(2)}</td>
           <td>
-            <button onClick={()=>this.deleteState(data)}>Delete</button>
+            <button onClick={()=>this.props.deleteState(data.id)}>Delete</button>
           </td>
         </tr>
       );
--- starter-code/src/ContactList.js
+++ starter-code/src/ContactList.js
@@ -4,15 +4,16 @@ import Contact from "./Contact";
 
 class ContactList extends Component {
   state = {
-    contact: [...contacts].slice(0, 5)
+    contact: contacts.slice(0, 5)
   };
 
   addRandom = () => {
     const randomIndex = Math.floor(Math.random() * 199);
-    const newContact = [...contacts][randomIndex];
+    const newContact = contacts[randomIndex];
 
     this.setState({
-      contact: this.state.contact.concat(newContact)
+      //   contact: this.state.contact.concat(newContact)
+      contact: [...this.state.contact, newContact]
     });
 
     console.log(this.state.contact);
@@ -28,16 +29,27 @@ class ContactList extends Component {
     });
   };
 
-  sortPopularity =()=>{
-      const sortedPopularity = [...this.state.contact].sort((a,b)=>{
-          return a.popularity - b.popularity
-      });
+  sortPopularity = () => {
+    const sortedPopularity = [...this.state.contact].sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
 
+    this.setState({
+      contact: sortedPopularity
+    });
+  };
+
+  deleteState = id => {
+    console.log(id);
+    let newArr = [...this.state.contact];
+    if (id !== -1) {
+      newArr.splice(id, 1);
+      console.log(newArr);
       this.setState({
-          contact: sortedPopularity
-      })
-
-  }
+        contact: newArr
+      });
+    }
+  };
 
   render() {
     return (
@@ -46,15 +58,19 @@ class ContactList extends Component {
         <button onClick={this.addRandom}>Add Random Contact</button>
         <button onClick={this.sortName}>Sort by name</button>
         <button onClick={this.sortPopularity}>Sort by popularity</button>
-
         <table>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Action</th>
-          </tr>
-          <Contact contact={this.state.contact} />
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+            <Contact
+              contact={this.state.contact}
+              deleteState={this.deleteState}
+            />
+          </tbody>
         </table>
       </div>
     );
