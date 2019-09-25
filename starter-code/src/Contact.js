925dcd3c0f6ce93ad14c84e3d5fa31aa03dea143
Ninette <ninette.adhikari@gmail.com>
2019-09-25 12:53:14 +0200

    without the bonus

---
+++ starter-code/src/Contact.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  state = {
+    contact: this.props.contact
+  };
+
+  deleteState = e => {
+
+    console.log(this.props.contact.indexOf(e))
+    // let deleteContact=[]
+    this.props.contact.slice().map((el, index, arr) => {
+      let deleteIndex = this.props.contact.indexOf(e)
+      if (deleteIndex !== 1) {
+        arr.splice(deleteIndex, 1);
+        this.setState({
+          contact: arr
+        });
+        // console.log(deleteContact)
+      }
+
+     });
+
+  }
+
+  render() {
+    const contact = this.props.contact.map(data => {
+      return (
+        <tr key={data.id}>
+          <td>
+            <img src={data.pictureUrl} height="200px" alt="" />
+          </td>
+          <td>{data.name}</td>
+          <td>{data.popularity}</td>
+          <td>
+            <button onClick={()=>this.deleteState(data)}>Delete</button>
+          </td>
+        </tr>
+      );
+    });
+
+    return <React.Fragment>{contact}</React.Fragment>;
+  }
+}
+
+export default Contact;
+
+// import React from 'react'
+
+// const Contact = (props) => {
+//    const contact=props.contact.map(data=>{
+
+//        return (
+//           <tr key={data.id}>
+//             <td>
+//                 <img src={data.pictureUrl} height= "200px" alt=""/>
+//             </td>
+//             <td>{data.name}</td>
+//             <td>{data.popularity}</td>
+//             {/* <td><button onClick={delete}>Delete</button></td> */}
+//           </tr>
+//        )
+//    })
+//    return (<React.Fragment>
+
+//        {contact}
+//    </React.Fragment>)
+
+// }
+// export default Contact
---
+++ starter-code/src/ContactList.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import contacts from "./contacts.json";
+import Contact from "./Contact";
+
+class ContactList extends Component {
+  state = {
+    contact: contacts.slice(0, 5)
+  };
+
+  addRandom = () => {
+    const randomIndex = Math.floor(Math.random() * 199);
+    const newContact = contacts[randomIndex];
+
+    this.setState({
+      contact: this.state.contact.concat(newContact)
+    });
+
+    console.log(this.state.contact);
+  };
+
+  sortName = () => {
+    const sortedContact = this.state.contact.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
+    this.setState({
+      contact: sortedContact
+    });
+  };
+
+  sortPopularity =()=>{
+      const sotedPopularity = this.state.contact.sort((a,b)=>{
+          return a.popularity - b.popularity
+      });
+
+      this.setState({
+          contact: sotedPopularity
+      })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>IronContacts</h1>
+        <button onClick={this.addRandom}>Add Random Contact</button>
+        <button onClick={this.sortName}>Sort by name</button>
+        <button onClick={this.sortPopularity}>Sort by popularity</button>
+
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          <Contact contact={this.state.contact} />
+        </table>
+      </div>
+    );
+  }
+}
+
+export default ContactList;
